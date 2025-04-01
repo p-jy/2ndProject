0ddd4e96f5e6105c435219626ef9efe69a9359f4
YDH2763 <dbehdgns12345@gmail.com>
2025-04-01 18:17:57 +0900

    모델추가(그룹 파트)

---
+++ src/main/java/kr/kh/spring/model/vo/GroupGoalVO.java
@@ -0,0 +1,11 @@
+package kr.kh.spring.model.vo;
+
+import lombok.Data;
+
+@Data
+public class GroupGoalVO {
+	
+	int gg_num;
+	int gg_gr_num; 
+	int gg_gl_num;
+}
---
+++ src/main/java/kr/kh/spring/model/vo/GroupMember.java
@@ -0,0 +1,11 @@
+package kr.kh.spring.model.vo;
+
+import lombok.Data;
+
+@Data
+public class GroupMember {
+
+	int gm_num; 
+	String gm_me_id;
+	int gm_gr_num; 
+}
